9779ff4f21aa9b79b7ef279647c15145f3b61384
Dwight <dwightscode@gmail.com>
2026-09-22 22:51:37 -0400

    added cloudflare wrangle and updated settings

--- api/config/settings.py
+++ api/config/settings.py
@@ -66,7 +66,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
-      "http://localhost:5173"  # React Vite Development Server
+      "http://localhost:5173",
+      "https://web.ddcattle.workers.dev",
+      "https://ddcattle.company"
   
 ]
 
