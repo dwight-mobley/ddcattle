e9078f64e3499c5e9273b062a3347e87ead6574d
Dwight <dwightscode@gmail.com>
2026-09-22 21:57:52 -0400

    updated allowed hosts

--- api/config/settings.py
+++ api/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=1$^=+n#ct1%&dlxqr#*i*j5&a_iia$_ese!0isl4l2*=is#jj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ddcattle.onrender.com']
 
 
 # Application definition
